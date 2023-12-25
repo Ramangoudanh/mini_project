85f5340689d4990d48b1f6ad379264c01f87172d
Ramangoudanh <ramangoudanh@gmail.com>
2023-12-25 21:14:42 +0530

    intialization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
 
 export default function App() {
-  return (
-   <h1 className='text-red-500'>app</h1>
-  )
+  return <BrowserRouter>
+         <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/signin" element={<Signin/>}></Route>
+          <Route path="/signup" element={<SignUp/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+         </Routes>
+  </BrowserRouter>
+  
+  
 }
