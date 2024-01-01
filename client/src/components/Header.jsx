0d43a48a943b1fbc5bab6d71e1160b8c3d275bc1
Ramangoudanh <ramangoudanh@gmail.com>
2024-01-01 15:56:10 +0530

    some ui changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,12 @@ import { useSelector } from 'react-redux';
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className='bg-slate-200'>
+    <div className='bg-black'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold'>Auth App</h1>
+          <h1 className='font-bold text-slate-100'>Auth App</h1>
         </Link>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-4 text-white'>
           <Link to='/'>
             <li>Home</li>
           </Link>
