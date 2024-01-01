4621ef8e82be99a0f5128ef21b0af9e538de02b0
Ramangoudanh <ramangoudanh@gmail.com>
2024-01-01 15:39:02 +0530

    adding google provider and complete project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Signin from "./pages/Signin";
-import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
-import Header from "./components/header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
-  return <BrowserRouter>
-         <Header/>
-         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/signin" element={<Signin/>}></Route>
-          <Route path="/signup" element={<SignUp/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
-         </Routes>
-  </BrowserRouter>
-  
-  
+  return (
+    <BrowserRouter>
+      {/* header */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
-import { set } from 'mongoose';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -29,6 +30,7 @@ export default function SignUp() {
         setError(true);
         return;
       }
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(true);
@@ -36,44 +38,44 @@ export default function SignUp() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-    <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-      <input
-        type='text'
-        placeholder='Username'
-        id='username'
-        className='bg-slate-100 p-3 rounded-lg'
-        onChange={handleChange}
-      />
-      <input
-        type='email'
-        placeholder='Email'
-        id='email'
-        className='bg-slate-100 p-3 rounded-lg'
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        placeholder='Password'
-        id='password'
-        className='bg-slate-100 p-3 rounded-lg'
-        onChange={handleChange}
-      />
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='Username'
+          id='username'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
         <button
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign Up'}
-      </button>
-    </form>
-    <div className='flex gap-2 mt-5'>
-      <p>Have an account?</p>
-      <Link to='/signin'>
-        <span className='text-blue-500'>Sign in</span>
-      </Link>
+        </button>
+        <OAuth />
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-500'>Sign in</span>
+        </Link>
+      </div>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
-    <p className='text-red-700 mt-5'>{error && '${error}'}</p>
-  </div>
-);
-  
+  );
 }
