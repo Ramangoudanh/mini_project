818b56b2ad2f1a53bb3dfb62b8aff674f5a2d2aa
Ramangoudanh <ramangoudanh@gmail.com>
2024-03-01 23:55:30 +0530

    added complaint list

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         
         <Link to='/'>
-          <h1 className='font-bold text-slate-100'>Auth App</h1>
+          <h1 className='font-bold text-slate-100'>Complaint App</h1>
         </Link>
         <ul className='flex gap-4 text-white'>
         <Link to='/complaints'>
