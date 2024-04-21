fed03647db9d75f365f8c404bb64defa5f80a854
Ramangoudanh <ramangoudanh@gmail.com>
2024-04-21 11:48:03 +0530

    small changes

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -13,7 +13,7 @@ export default function OAuth() {
       const auth = getAuth(app);
 
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch('/api/auth/google', {
+      const res = await fetch('https://mini-project-fo4m.onrender.com/api/auth/google', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
