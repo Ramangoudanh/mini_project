95370ecba050574c9dde678e7ee224759dee0315
Ramangoudanh <ramangoudanh@gmail.com>
2024-04-21 11:30:39 +0530

    locl to cloud

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://mini-project-fo4m.onrender.com',
         secure: false,
       },
     },
