544882d364357e9a84307a3ee3491994d3eb0557
Ramangoudanh <ramangoudanh@gmail.com>
2024-04-29 14:49:33 +0530

    tiny change

--- client/src/components/proChart.jsx
+++ client/src/components/proChart.jsx
@@ -7,7 +7,7 @@ const ProChart = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:3000/api/complaint/getComplaintStatusData");
+                const response = await fetch("https://mini-project-fo4m.onrender.com/api/complaint/getComplaintStatusData");
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
